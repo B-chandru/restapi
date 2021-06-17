e5554ffc3e71aa0fd84ba3737828abe89f38af42
Chandru <65664480+B-chandru@users.noreply.github.com>
2021-06-17 19:58:34 +0530

    Update userRoutes.js

--- userRoutes.js
+++ userRoutes.js
@@ -62,7 +62,7 @@ router.delete("/:id",async (req,res) => {
         res.status(200).json(deleteusers);
        }
 catch(err){
-    res.status(500)json({"err":err});
+    res.status(500).json({"err":err});
 }
 });
 
